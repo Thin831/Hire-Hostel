da91e3772fb25942b9913572386138f2c826773e
Thin831 <thinthazin.kme258@gmail.com>
2019-09-10 16:30:24 +0630

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/webhook', (req, res) => {
       		"recipient":{
       			"id":userid
       		},
-      		"messages":{
+      		"message":{
       			"attachment":{
       				"type":"template",
       				"payload":{
@@ -86,6 +86,10 @@ app.post('/webhook', (req, res) => {
       				}
       			}
       		}
+      	}).then(function(success){
+      		console.log('success');
+      	}).fail(function(error){
+      		console.log('error', error);
       	})
       }
 
